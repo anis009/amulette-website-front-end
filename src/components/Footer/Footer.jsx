33fa1c3bea65f5afd3645fcd0a5b5063e5e74d9a
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2024-02-05 03:13:48 +0600

    stay with us position absolute in mobile

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,11 +6,11 @@ import StayWithUs from "./StayWithUs/StayWithUs";
 const Footer = () => {
 	// stay-with-us-wrapper 
 	return (
-		<div className="footer  mx-auto">
-			<div className="stay-with-us-wrapper w-full mt-[50px] lg:mt-[74px] desktop hidden lg:block">
+		<div className="footer  mx-auto relative">
+			<div className="stay-with-us-wrapper w-full mt-[50px] lg:mt-[74px] desktop absolute lg:static z-50 top-[-190px]">
 				<StayWithUs />
 			</div>
-			<div className="footer-wrapper relative bg-[#25282B] lg:pt-[60px] lg:pb-[60px] mb-0">
+			<div className="footer-wrapper relative bg-[#25282B] pt-[60px] lg:pb-[60px] mb-0 mt-[140px] lg:mt-0">
 				<div className="container max-w-amulette lg:px-amulette mx-auto px-0 footer-main flex flex-col lg:flex-row items-center lg:items-start justify-between text-white">
 					<DesktopFooter />
 					<MobileFooter />
--- src/components/Footer/StayWithUs/StayWithUs.jsx
+++ src/components/Footer/StayWithUs/StayWithUs.jsx
@@ -4,7 +4,7 @@ const StayWithUs = () => {
 	return (
 		<div
 			style={{
-				backgroundImage: "url('Images/StayWithUs/Stay-With-Us.svg')",
+				// backgroundImage: "",
 				backgroundPosition: "bottom left",
 				width: "100%",
 				// height: "400px", // Adjust the height as needed
@@ -12,7 +12,7 @@ const StayWithUs = () => {
 				backgroundSize: "contain",
 				backgroundRepeat: "no-repeat",
 			}}
-			className="py-8 lg:pb-16 "
+			className="py-8 lg:pb-16 bg-none lg:bg-[url('Images/StayWithUs/Stay-With-Us.svg')]"
 		>
 			<div className="container stay-with-us max-w-amulette lg:px-amulette mx-auto   ">
 				<div className="stay-with-us-inner flex flex-col lg:flex-row items-center justify-center lg:justify-between bg-[#FFEFE8] rounded-2xl py-8 lg:py-14 px-10 lg:px-16 ">
@@ -20,11 +20,11 @@ const StayWithUs = () => {
 						<h1 className="text-[#1B1B1B] text-xl text-center lg:text-left  lg:text-4xl font-Poppins font-bold lg:font-semibold">
 							Stay with us
 						</h1>
-						<p className="text-[#1B1B1B] font-Poppins lg:text-left text-center text-sm lg:text-lg font-normal mt-3 sm:pl-0 pl-2">
+						<p className="text-[#1B1B1B] font-Poppins lg:text-left text-center text-sm lg:text-lg font-normal mt-3 sm:pl-0 pl-2 mb-5 lg:mb-0">
 							Subscribe and be ready for an amazing experience
 						</p>
 					</div>
-					<div className="lg:w-1/2 w-full flex content-end self-end justify-end">
+					<div className="lg:w-1/2 w-full flex content-center lg:content-end self-center lg:self-end justify-center lg:justify-end mt-5 lg:mt-0">
 						<div className="email-subscribe w-auto rounded-lg flex flex-row items-baseline justify-end content-end space-x-3 p-1.5 bg-white ">
 							<input
 								className="border-collapse  focus:border-collapse px-4 py-3 rounded-md"
