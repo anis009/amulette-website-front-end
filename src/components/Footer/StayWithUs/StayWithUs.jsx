7b2a34596ebaabd1f32b6638a92e49a59d39f8fb
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2024-02-05 13:09:03 +0600

    stay with us overflow fixed

--- src/components/Footer/StayWithUs/StayWithUs.jsx
+++ src/components/Footer/StayWithUs/StayWithUs.jsx
@@ -15,7 +15,7 @@ const StayWithUs = () => {
 			className="py-8 lg:pb-16 bg-none lg:bg-[url('/Images/StayWithUs/Stay-With-Us.svg')]"
 		>
 			<div className="container stay-with-us max-w-amulette lg:px-amulette mx-auto   ">
-				<div className="stay-with-us-inner flex flex-col lg:flex-row items-center justify-center lg:justify-between bg-[#FFEFE8] rounded-2xl py-8 lg:py-14 px-10 lg:px-16 ">
+				<div className="stay-with-us-inner flex flex-col lg:flex-row items-center justify-center lg:justify-between bg-[#FFEFE8] rounded-2xl py-8 lg:py-14 px-5 sm:px-10 lg:px-16 ">
 					<div className="lg:w-1/2 w-full">
 						<h1 className="text-[#1B1B1B] text-xl text-center lg:text-left  lg:text-4xl font-Poppins font-bold lg:font-semibold">
 							Stay with us
@@ -24,10 +24,10 @@ const StayWithUs = () => {
 							Subscribe and be ready for an amazing experience
 						</p>
 					</div>
-					<div className="lg:w-1/2 w-full flex content-center lg:content-end self-center lg:self-end justify-center lg:justify-end mt-5 lg:mt-0">
-						<div className="email-subscribe w-auto rounded-lg flex flex-row items-baseline justify-end content-end space-x-3 p-1.5 bg-white ">
+					<div className="lg:w-1/2 flex lg:content-end lg:self-end lg:justify-end mt-5 lg:mt-0">
+						<div className="email-subscribe w-full max-w-full rounded-lg flex flex-col sm:flex-row sm:items-baseline sm:justify-end sm:content-end space-x-3 p-1.5 space-y-3 bg-white ">
 							<input
-								className="border-collapse  focus:border-collapse px-4 py-3 rounded-md"
+								className="border-collapse focus:outline-none focus:border-collapse px-4 py-3 rounded-md"
 								type="email"
 								placeholder="Enter email address"
 							/>
