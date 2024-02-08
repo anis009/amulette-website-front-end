7ed4ab7970fcb7773acf13c3d1a3f7598a4b8ccf
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2024-02-08 09:36:38 +0600

    prodcut card mobile spacing, with fixed

--- src/components/Home/OurProducts/ProductCard.jsx
+++ src/components/Home/OurProducts/ProductCard.jsx
@@ -14,7 +14,7 @@ const ProductCard = ({ product, setShowModal, ourCardHomeCarousel, index }) => {
 	return (
 		<div
 			className={classNames(
-				"flex flex-col py-5 mx-2 border border-black product-card border-opacity-20 rounded-3xl text-center",
+				"flex flex-col py-5 mx-2 border border-black product-card border-opacity-20 rounded-3xl text-center h-full justify-between",
 				{
 					// "w-[200px]": ourCardHomeCarousel === true,
 				}
@@ -28,8 +28,8 @@ const ProductCard = ({ product, setShowModal, ourCardHomeCarousel, index }) => {
 			>
 				<div
 					className={classNames(
-						"img-parent overflow-hidden h-[120px] w-full lg:h-[221px] lg:w-auto",
-						{}
+						"img-parent overflow-hidden w-full lg:w-auto flex justify-center",
+						{}//h-[120px] lg:h-[221px]
 					)}
 				>
 					<img
@@ -50,15 +50,14 @@ const ProductCard = ({ product, setShowModal, ourCardHomeCarousel, index }) => {
 			<p className="desc text-[9px] lg:text-xs text-[#1C121F] font-normal font-Poppins mt-2 px-5 text-left">
 				{product.shortDescription}
 			</p>
-			<div className="usd-add-to-cart flex flex-row mx-5 items-center justify-between space-x-2 mt-3.5">
-				<span className="text-[#1C121F] text-xs  lg:text-lg font-semibold">
+			<div className="usd-add-to-cart flex flex-col sm:flex-row mx-5 items-center justify-between sm:space-x-5 mt-3.5">
+				<span className="text-[#1C121F] text-base  lg:text-lg font-semibold">
 					${product.price}
 				</span>
-				<button className="bg-primaryColor text-white px-5 lg:px-10 py-1.5 lg:py-3.5 rounded-[50px]">
+				<button className="bg-primaryColor text-white w-full sm:w-auto lg:w-full sm:px-10 lg:px-0 py-1.5 lg:py-3.5 rounded-[50px] mt-2 sm:mt-0">
 					<div className="flex flex-row items-center justify-center space-x-1.5">
-						{" "}
 						<IoCartOutline className="h-4 w-4" size={16} />{" "}
-						<span className="text-[7px] font-medium lg:text-sm">Buy Now</span>
+						<span className="text-xs font-medium lg:text-sm">Buy Now</span>
 					</div>
 				</button>
 			</div>
