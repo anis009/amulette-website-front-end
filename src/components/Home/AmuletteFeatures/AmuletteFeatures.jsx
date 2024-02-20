0e678dfe8dec972c6f591d6b4dfbf7ef9563c9bf
anichu <anis.cse5.bu@gmail.com>
2024-02-20 08:05:54 +0600

    amulette features

--- src/components/Home/AmuletteFeatures/AmuletteFeatures.jsx
+++ src/components/Home/AmuletteFeatures/AmuletteFeatures.jsx
@@ -1,51 +1,59 @@
-import React from "react";
+// import React from "react";
 import AmuletteServiceCard from "./AmuletteServiceCard";
+import lottieFiles1 from "../../../assets/lottieflies/Flow 1.json";
+import lottieFiles2 from "../../../assets/lottieflies/Flow 6.json";
+import lottieFiles3 from "../../../assets/lottieflies/Flow 3.json";
 
 const AmuletteFeatures = () => {
-  const amuletteServices = [
-    {
-      title: 'Connect with Ease',
-      description: 'Seamless Bluetooth pairing with our Amulette app. Connect and share moments effortlessly',
-      image: 'Images/AmuletteFeatures/Ease.svg'
+	const amuletteServices = [
+		{
+			title: "Connect with Ease",
+			description:
+				"Seamless Bluetooth pairing with our Amulette app. Connect and share moments effortlessly",
+			image: "Images/AmuletteFeatures/Ease.svg",
+			src: lottieFiles1,
+		},
+		{
+			title: "Display Your Personality",
+			description:
+				"Wearable, compact, and customizable – let your style shine with Amulette",
+			image: "Images/AmuletteFeatures/Personality.svg",
+			src: lottieFiles2,
+		},
+		{
+			title: "Share and Explore",
+			description:
+				"Share images with friends and create lasting memories. Explore a world of creativity together",
+			image: "Images/AmuletteFeatures/Explore.svg",
+			src: lottieFiles3,
+		},
+	];
 
-    },
-    {
-      title: 'Display Your Personality',
-      description: 'Wearable, compact, and customizable – let your style shine with Amulette',
-      image: 'Images/AmuletteFeatures/Personality.svg'
-
-    },
-    {
-      title: 'Share and Explore',
-      description: 'Share images with friends and create lasting memories. Explore a world of creativity together',
-      image: 'Images/AmuletteFeatures/Explore.svg'
-
-    }
-
-  ];
-  return (
-    <div style={{ backgroundColor: 'rgba(246, 95, 25, 0.10)' }} className="amulette-service-wrapper py-14">
-      <div className="amulette-service custom-container">
-        <h1 className="text-xl lg:text-[40px] font-semibold font-Poppins text-center">
-          <span className="text-primaryColor">Amulette </span>
-          <span className="">
-            Features
-          </span>
-        </h1>
-        <p style={{ color: 'color: rgba(0, 0, 0, 0.80)' }} className="about-feature text-center text-sm sm:text-base lg:text-xl font-normal text-menuColor mt-4 mb-10 lg:mb-0">
-          Empowering individuals to express themselves creatively through
-          wearable technology.
-        </p>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:mt-[60px]">
-          {amuletteServices?.map((service, index) => (
-            <AmuletteServiceCard key={index} service={service} />)
-          )}
-
-
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			style={{ backgroundColor: "rgba(246, 95, 25, 0.10)" }}
+			className="amulette-service-wrapper py-14"
+		>
+			<div className="amulette-service custom-container">
+				<h1 className="text-xl lg:text-[40px] font-semibold font-Poppins text-center">
+					<span className="text-primaryColor">Amulette </span>
+					<span className="">Features</span>
+				</h1>
+				<p
+					style={{ color: "color: rgba(0, 0, 0, 0.80)" }}
+					className="mt-4 mb-10 text-sm font-normal text-center about-feature sm:text-base lg:text-xl text-menuColor lg:mb-0"
+				>
+					Empowering individuals to express themselves creatively through
+					wearable technology.
+				</p>
+				<div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:mt-[60px]">
+					{amuletteServices?.map((service, index) => (
+						<AmuletteServiceCard key={index} service={service} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default AmuletteFeatures;
--- src/components/Home/AmuletteFeatures/AmuletteServiceCard.jsx
+++ src/components/Home/AmuletteFeatures/AmuletteServiceCard.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
 
 const AmuletteServiceCard = ({ service }) => {
-  return (
-    <div className="service-card flex flex-col items-center justify-center mb-10 lg:mb-0">
-      <img src={`${service.image}`} alt={service.title} />
-      <h2 className="text-center text-lg lg:text-xl xl:text-2xl font-semibold text-black mt-3">
-        {service.title}
-      </h2>
-      <p className="text-center text-sm md:text-base lg:text-lg xl:text-xl font-normal leading-5 lg:leading-7 xl:leading-9 mt-2.5">
-        {service.description}
-      </p>
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center mb-10 service-card lg:mb-0">
+			{/* <img src={`${service.image}`} alt={service.title} /> */}
+			<Player
+				autoplay
+				loop
+				src={service.src}
+				style={{ height: "300px", width: "300px" }}
+			>
+				{/* <Controls
+					visible={true}
+					buttons={["play", "repeat", "frame", "debug"]}
+				/> */}
+			</Player>
+			<h2 className="mt-3 text-lg font-semibold text-center text-black lg:text-xl xl:text-2xl">
+				{service.title}
+			</h2>
+			<p className="text-center text-sm md:text-base lg:text-lg xl:text-xl font-normal leading-5 lg:leading-7 xl:leading-9 mt-2.5">
+				{service.description}
+			</p>
+		</div>
+	);
 };
 
 export default AmuletteServiceCard;
